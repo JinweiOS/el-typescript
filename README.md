0a80b9bc2aa0ac5de1c4a1fb04dea2547ed3e1ef
Jinwei <105687126+JinweiOS@users.noreply.github.com>
2022-10-08 16:01:30 +0800

    feat: ts-node

---
+++ README.md
@@ -0,0 +1,13 @@
+# typescript
+
+## 数据类型
+
+[见src/data-types.ts](./src/data-type.ts)
+
+## 工程
+
+### 如何在前端使用
+
+### 对于一个Node.js项目，如何使用
+
+## 模块和全局变量声明
---
+++ src/index.ts
@@ -0,0 +1,42 @@
+// import fs from 'fs/promises'
+// import path from 'path'
+
+// async function main() {
+//     const data = await fs.readFile(path.join(__dirname, '../package.json'));
+//     console.log(data.toString('utf8'))
+// }
+
+// main()
+
+// import koa from 'koa'
+
+// const server = new koa();
+
+// server.use((context) => {
+//     context.body = 'hello'
+// });
+
+// server.listen(3000, () => { console.log('server is listening on port 3000!') })
+
+
+import Fastify from 'fastify'
+
+const server = Fastify({
+    logger: true
+})
+
+server.get('/hello', (res, rep) => {
+    rep.send('hello fastify')
+})
+
+async function fatisfyMain() {
+    await server.listen({
+        port: 3000,
+        host: '127.0.0.1',
+    })
+    console.log('server is listening on 127.0.0.1:3000')
+}
+fatisfyMain()
+
+
+
